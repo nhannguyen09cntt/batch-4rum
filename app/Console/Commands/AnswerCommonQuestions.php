2812b4257a7448624276d725ea84ce0b21f3f622
Nguyen Van Trong Nhan <nhannvt@lifull-tech.vn>
2019-12-25 13:31:31 +0700

    Add console command question:answer

--- app/Console/Commands/AnswerCommonQuestions.php
+++ app/Console/Commands/AnswerCommonQuestions.php
@@ -2,23 +2,31 @@
 
 namespace App\Console\Commands;
 
+use Discourse\Discourse;
+use GuzzleHttp\Client;
 use Illuminate\Console\Command;
 
 class AnswerCommonQuestions extends Command
 {
+    const PAGE_LIMIT = 2;
+
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    protected $signature = 'question:answer';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Auto-answer common questions at 4rum.vn';
+
+    private $discourse;
+
+    private $botAnswerClient;
 
     /**
      * Create a new command instance.
@@ -27,6 +35,8 @@ class AnswerCommonQuestions extends Command
      */
     public function __construct()
     {
+        $this->botAnswerClient = new Client();
+        $this->discourse = new Discourse();
         parent::__construct();
     }
 
@@ -37,6 +47,37 @@ class AnswerCommonQuestions extends Command
      */
     public function handle()
     {
-        //
+
+        for ($page = 1; $page < self::PAGE_LIMIT; $page++) {
+            $response = $this->discourse->get('/c/11.json', ['page' => $page]);
+            $body = $response->getDecodedBody();
+            if (empty($body['topic_list'])) {
+                break;
+            }
+            $topics = $body['topic_list']['topics'];
+
+            foreach ($topics as $topic) {
+                $this->sendAnswer($topic);
+            }
+        }
+    }
+
+    private function sendAnswer($topic)
+    {
+        if ($topic['posts_count'] > 1 || $topic['last_poster_username'] != 'kimnguyet0815') {
+            return;
+        }
+        $title = $topic['title'];
+        $answer = '';
+
+        $answerRes = $this->botAnswerClient->request('POST', 'http://localhost:8080', [
+            'body' => $title,
+        ]);
+        $stringBody = (string) $answerRes->getBody();
+
+        echo "Question: " . $title;
+        echo "\n";
+        echo "Answer: ";
+        var_dump($stringBody);
     }
 }
